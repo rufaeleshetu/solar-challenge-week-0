aaad93474b025962c45cc9b021fd386ce610f827
Rufael Eshetu Yigezu <zerufeshetu121@gmail.com>
2025-05-19 20:51:04 +0300

    style: fix flake8 E302 and W292 issues

--- src/eda_utils.py
+++ src/eda_utils.py
@@ -2,12 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-
 class SolarDataAnalyzer:
     """
     A utility class for loading and analyzing solar data.
     """
-
 
     def __init__(self, filepath):
         """
@@ -18,26 +16,18 @@ class SolarDataAnalyzer:
         """
         self.df = pd.read_csv(filepath)
 
-
     def plot_column(self, column, title="Column Over Time"):
         """
         Plot a time series line graph for a given column.
-
-        Args:
-            column (str): Column name to plot.
-            title (str): Title of the plot.
         """
-        plt.plot(self.df[column])
-        plt.title(title)
-        plt.xlabel("Time")
-        plt.ylabel(column)
+        plt.plot(self.df[column], title=title)
         plt.tight_layout()
         plt.show()
-
 
     def summary_stats(self):
         """
         Return summary statistics of the dataset.
         """
         return self.df.describe()
+    
     
